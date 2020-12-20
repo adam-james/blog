689316694b6e400115d365eac3a134cc3d1f80a6
adam-james <adam.j.sulewski@gmail.com>
2020-12-20 14:59:06 -0800

    add i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    locale = params[:locale] || cookies[:locale]
+    locale = I18n.default_locale unless I18n.available_locales.include?(locale&.to_sym)
+    expiry = Time.now + 30.days
+    cookies[:locale] = { value: locale, expires: expiry, same_site: :lax }
+    I18n.locale = locale
+  end
 end
