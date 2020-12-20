4e61ede7681652534258dc22e2b2f55e55748215
adam-james <adam.j.sulewski@gmail.com>
2020-12-20 14:25:00 -0800

    home routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,10 @@ class HomeController < ApplicationController
   def index
     @posts = Post.order(created_at: :desc).page(params[:page])
   end
+
+  def show
+    # TODO use date?
+    # Mongoid throws not found if passed as is.
+    @post = Post.find_by(id: params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
-  resources :home, only: [:index, :show]
+  get '/', to: 'home#index', format: true
+  get '/:date/:id', to: 'home#show'
 end
